f54daf8cfe3afa6704d5bc5167e67c83603d598d
Antonio Fernandes <devantonio.fer@gmail.com>
2024-04-21 01:21:26 -0300

    wip: posts feature

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<int:id_post>', post),
+    path('<int:id_post>', post_page, name="post_page"),
 ]
--- src/apps/posts/views.py
+++ src/apps/posts/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from .models import Post
 
 
-def post(request, id_post):
+def post_page(request, id_post):
     post_data = Post.objects.get(id=id_post)
-    print(post_data, id_post)
-    return render(request, 'pages/index.html', {
+    return render(request, 'pages/post.html', {
         'post': post_data
     })
