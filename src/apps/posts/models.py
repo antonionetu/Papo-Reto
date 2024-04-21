7ab15e52dfc1d6c2563be68640f3e67a20b4725f
Antonio Fernandes <devantonio.fer@gmail.com>
2024-04-20 21:12:24 -0300

    feat: starting posts app

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class NameMixin(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class PostbleMixin(TimeStampMixin):
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Author(TimeStampMixin, NameMixin):
+    born_date = models.DateField()
+
+
+class Post(PostbleMixin):
+    title = models.CharField(max_length=255)
+    viewers_count = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        Author,
+        related_name='posts',
+        on_delete=models.PROTECT
+    )
+
+
+class Tag(NameMixin):
+    ...
+
+
+class Comment(PostbleMixin):
+    parent_id = models.IntegerField()
+    author = models.ForeignKey(
+        Author,
+        related_name='comments',
+        on_delete=models.PROTECT
+    )
--- src/apps/posts/views.py
+++ src/apps/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
-def post(request, id_post):    
+def post(request, id_post):
+    post_data = Post.objects.get(id=id_post)
+    print(post_data, id_post)
     return render(request, 'pages/index.html', {
-        'id_post': id_post,
-        'title': 'Um post legal!'
+        'post': post_data
     })
